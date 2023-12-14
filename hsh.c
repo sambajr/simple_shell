8bff911b73ce060c66ae6406ae37df4145ea66ca
sambajr <metasamba.ng007@gmail.com>
2023-12-14 02:40:53 +0100

    shell

--- hsh.c
+++ hsh.c
@@ -7,7 +7,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-char *find_in_path(char *command) {
+extern char **environ;
+
+/**
+ * find_in_path - find command in PATH
+ * @command: command to find
+ *
+ * Return: full path to command or NULL if not found
+ */
+char *find_in_path(char *command)
+{
     struct stat st;
     char *path = getenv("PATH");
     char *path_copy;
@@ -15,49 +24,62 @@ char *find_in_path(char *command) {
     char *fullpath = malloc(1024);
 
     /* If command starts with '/', it's an absolute path */
-    if (command[0] == '/') {
-        if (stat(command, &st) == 0 && st.st_mode & S_IXUSR) {
-            return strdup(command);
-        } else {
-            return NULL;
-        }
+    if (command[0] == '/')
+    {
+        if (stat(command, &st) == 0 && st.st_mode & S_IXUSR)
+            return (strdup(command));
+        else
+            return (NULL);
     }
 
     /* Otherwise, search for the command in the PATH */
     path_copy = strdup(path);
     p = strtok(path_copy, ":");
 
-    while (p != NULL) {
+    while (p != NULL)
+    {
         sprintf(fullpath, "%s/%s", p, command);
-        if (stat(fullpath, &st) == 0 && st.st_mode & S_IXUSR) {
+        if (stat(fullpath, &st) == 0 && st.st_mode & S_IXUSR)
+        {
             free(path_copy);
-            return fullpath;
+            return (fullpath);
         }
         p = strtok(NULL, ":");
     }
 
     free(path_copy);
     free(fullpath);
-    return NULL;
+    return (NULL);
 }
 
-
-
-char **split_line(char *line) {
+/**
+ * split_line - split line into tokens
+ * @line: line to split
+ *
+ * Return: array of tokens
+ */
+char **split_line(char *line)
+{
     char **tokens = malloc(64 * sizeof(char *));
     char *token;
     int position = 0;
 
     token = strtok(line, " ");
-    while (token != NULL) {
+    while (token != NULL)
+    {
         tokens[position] = token;
         position++;
         token = strtok(NULL, " ");
     }
     tokens[position] = NULL;
-    return tokens;
+    return (tokens);
 }
 
+/**
+ * main - main function for the shell
+ *
+ * Return: 0 on success
+ */
 int main(void) {
     char *line = NULL;
     size_t len = 0;
@@ -66,24 +88,43 @@ int main(void) {
     char *command;
     pid_t pid;
     int status;
-
+    int i;
     while (1) {
-        printf("$ ");
-        fflush(stdout);
+        write(1, "$ ", 2);
         read = getline(&line, &len, stdin);
 
         if (read == -1) {
-            printf("\n");
+            write(1, "\n", 1);
             free(line);
             exit(EXIT_SUCCESS);
         }
 
         line[read - 1] = '\0';
         args = split_line(line);
+
+        /* Check for 'exit' command */
+        if (strcmp(args[0], "exit") == 0) {
+            free(args);
+            free(line);
+            exit(EXIT_SUCCESS);
+        }
+
+        /* Check for 'env' command */
+        if (strcmp(args[0], "env") == 0) {
+            for (i = 0; environ[i]; i++) {
+                write(1, environ[i], strlen(environ[i]));
+                write(1, "\n", 1);
+            }
+            free(args);
+            continue;
+        }
+
         command = find_in_path(args[0]);
 
         if (command == NULL) {
-            fprintf(stderr, "Error: Command '%s' not found in PATH\n", args[0]);
+            write(2, "Error: Command '", 16);
+            write(2, args[0], strlen(args[0]));
+            write(2, "' not found in PATH\n", 19);
             free(args);
             continue;
         }
@@ -107,6 +148,5 @@ int main(void) {
     }
 
     free(line);
-    return EXIT_SUCCESS;
+    return (0);
 }
-
