8cb88e16946199e2014be0d50eaadd266932a4f4
sambajr <metasamba.ng007@gmail.com>
2023-12-14 03:06:56 +0100

    shell

--- hsh.c
+++ hsh.c
@@ -6,8 +6,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "main.h"
 
-extern char **environ;
 
 /**
  * find_in_path - find command in PATH
@@ -17,39 +17,39 @@ extern char **environ;
  */
 char *find_in_path(char *command)
 {
-    struct stat st;
-    char *path = getenv("PATH");
-    char *path_copy;
-    char *p;
-    char *fullpath = malloc(1024);
+	struct stat st;
+	char *path = getenv("PATH");
+	char *path_copy;
+	char *p;
+	char *fullpath = malloc(1024);
 
     /* If command starts with '/', it's an absolute path */
-    if (command[0] == '/')
-    {
-        if (stat(command, &st) == 0 && st.st_mode & S_IXUSR)
-            return (strdup(command));
-        else
-            return (NULL);
-    }
+	if (command[0] == '/')
+	{
+		if (stat(command, &st) == 0 && st.st_mode & S_IXUSR)
+			return (strdup(command));
+		else
+			return (NULL);
+	}
 
     /* Otherwise, search for the command in the PATH */
-    path_copy = strdup(path);
-    p = strtok(path_copy, ":");
+	path_copy = strdup(path);
+	p = strtok(path_copy, ":");
 
-    while (p != NULL)
-    {
-        sprintf(fullpath, "%s/%s", p, command);
-        if (stat(fullpath, &st) == 0 && st.st_mode & S_IXUSR)
-        {
-            free(path_copy);
-            return (fullpath);
-        }
-        p = strtok(NULL, ":");
-    }
+	while (p != NULL)
+	{
+		sprintf(fullpath, "%s/%s", p, command);
+		if (stat(fullpath, &st) == 0 && st.st_mode & S_IXUSR)
+		{
+			free(path_copy);
+			return (fullpath);
+		}
+		p = strtok(NULL, ":");
+	}
 
-    free(path_copy);
-    free(fullpath);
-    return (NULL);
+	free(path_copy);
+	free(fullpath);
+	return (NULL);
 }
 
 /**
@@ -60,19 +60,19 @@ char *find_in_path(char *command)
  */
 char **split_line(char *line)
 {
-    char **tokens = malloc(64 * sizeof(char *));
-    char *token;
-    int position = 0;
+	char **tokens = malloc(64 * sizeof(char *));
+	char *token;
+	int position = 0;
 
-    token = strtok(line, " ");
-    while (token != NULL)
-    {
-        tokens[position] = token;
-        position++;
-        token = strtok(NULL, " ");
-    }
-    tokens[position] = NULL;
-    return (tokens);
+	token = strtok(line, " ");
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+		token = strtok(NULL, " ");
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
 
 /**
@@ -80,73 +80,83 @@ char **split_line(char *line)
  *
  * Return: 0 on success
  */
-int main(void) {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    char **args;
-    char *command;
-    pid_t pid;
-    int status;
-    int i;
-    while (1) {
-        write(1, "$ ", 2);
-        read = getline(&line, &len, stdin);
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char **args;
+	char *command;
+	pid_t pid;
+	int status;
+	int i;
 
-        if (read == -1) {
-            write(1, "\n", 1);
-            free(line);
-            exit(EXIT_SUCCESS);
-        }
+	while (1)
+	{
+		write(1, "$ ", 2);
+		read = getline(&line, &len, stdin);
 
-        line[read - 1] = '\0';
-        args = split_line(line);
+		if (read == -1)
+		{
+			write(1, "\n", 1);
+			free(line);
+			exit(EXIT_SUCCESS);
+		}
 
-        /* Check for 'exit' command */
-        if (strcmp(args[0], "exit") == 0) {
-            free(args);
-            free(line);
-            exit(EXIT_SUCCESS);
-        }
+		line[read - 1] = '\0';
+		args = split_line(line);
+		/* Check exit */
+		if (strcmp(args[0], "exit") == 0)
+		{
+			free(args);
+			free(line);
+			exit(EXIT_SUCCESS);
+		}
+		/* Check env */
+		if (strcmp(args[0], "env") == 0)
+		{
+			for (i = 0; environ[i]; i++)
+			{
+				write(1, environ[i], strlen(environ[i]));
+				write(1, "\n", 1);
+			}
+			free(args);
+			continue;
+		}
 
-        /* Check for 'env' command */
-        if (strcmp(args[0], "env") == 0) {
-            for (i = 0; environ[i]; i++) {
-                write(1, environ[i], strlen(environ[i]));
-                write(1, "\n", 1);
-            }
-            free(args);
-            continue;
-        }
+		command = find_in_path(args[0]);
 
-        command = find_in_path(args[0]);
+		if (command == NULL)
+		{
+			write(2, "Error: Command '", 16);
+			write(2, args[0], strlen(args[0]));
+			write(2, "' not found in PATH\n", 19);
+			free(args);
+			continue;
+		}
 
-        if (command == NULL) {
-            write(2, "Error: Command '", 16);
-            write(2, args[0], strlen(args[0]));
-            write(2, "' not found in PATH\n", 19);
-            free(args);
-            continue;
-        }
-
-        pid = fork();
-        if (pid == 0) {
-            if (execve(command, args, NULL) == -1) {
-                perror("Error:");
-            }
-            exit(EXIT_FAILURE);
-        } else if (pid < 0) {
-            perror("Error:");
-        } else {
-            do {
-                waitpid(pid, &status, WUNTRACED);
-            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
-
-        free(args);
-        free(command);
-    }
-
-    free(line);
-    return (0);
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execve(command, args, NULL) == -1)
+			{
+				perror("Error:");
+			}
+			exit(EXIT_FAILURE);
+		}
+		else if (pid < 0)
+		{
+			perror("Error:");
+		}
+		else
+		{
+			do {
+				waitpid(pid, &status, WUNTRACED);
+			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		}
+		free(args);
+		free(command);
+	}
+	free(line);
+	return (0);
 }
--- main.h
+++ main.h
@@ -7,22 +7,10 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX_ARGS 10  /*Adjust the maximum number of arguments as needed*/
+#define MAX_ARGS 10
 
-/* Function to execute a command*/
-void execute_command(char *args[]);
 extern char **environ;
-/*Function to process user input*/
-char *get_user_input(void);
 
-/* Function to parse user input into arguments*/
-void parse_input(char *input, char *args[]);
-
-/*Function to check and handle the exit command*/
-void check_exit_command(char *args[]);
-void interactive_mode(int isatty_flag);
-void non_interactive_mode(const char *script);
-void check_env_command(char *args[]);
 
 
 #endif /* MAIN_H */
